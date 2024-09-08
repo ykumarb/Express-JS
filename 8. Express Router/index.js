f1ec02f33cbf957ecc981d402febd128abdd8e3b
Yupindra Kumar (CW) <ykumar@synaptics.com>
2024-09-08 14:51:01 +0530

    [Expres-JS]: Committing the learnt code to use express router and route params
    CL:
    	1. Import/export of route paths using router
    	2. Route param to check existence of url param in callbacks

---
+++ 8. Express Router/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import student from "./routes/student.js";
+
+const app = express();
+
+app.use('./student', student);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
